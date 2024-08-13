c7aa23f355c7a5209eca9ec8b0c08b5d2e1a42e4
bryantnguyen1801 <binh.nguyen@setel.com>
2024-08-13 18:47:37 +0700

    code learning up-to-date

---
+++ sdetpro_android/android_learning/src/test/java/learning/driver/AndroidCapabilityType.java
@@ -0,0 +1,12 @@
+package learning.driver;
+
+import io.appium.java_client.android.options.UiAutomator2Options;
+
+public class AndroidCapabilityType extends UiAutomator2Options {
+    public static final String PLATFORM_NAME = "platformName";
+    public static final String DEVICE_NAME = "deviceName";
+    public static final String AUTO_ACCEPT_ALERTS = "autoAcceptAlerts";
+    public static final String WAIT_FOR_IDLE_TIMEOUT_SETTINGS = "settings[waitForIdleTimeout]";
+    public static final String SYSTEM_PORT = "systemPort";
+
+}
---
+++ sdetpro_android/android_learning/src/test/java/learning/tests/testng/Verifier.java
@@ -0,0 +1,9 @@
+package learning.tests.testng;
+
+public class Verifier {
+    public static void verifyEquals(String actualResults, String expectedResults) {
+        if (!actualResults.equalsIgnoreCase(expectedResults)) {
+            throw new AssertionError("Expected value and actual value are different");
+        }
+    }
+}
